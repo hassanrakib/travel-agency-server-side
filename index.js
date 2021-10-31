cd748fdd57d65ee9eaa3667140013325a2170a99
rakib <rakiboe@gmail.com>
2021-10-31 21:39:07 +0600

    'got-you'

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
-const port = process.env.PORT || 5000;
+
 
 
 
 // middlewear
 app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT || 5000;
 
 const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u5kcu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -90,7 +92,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.end("INFO: SERVER IS UP AND RUNNING");
+    res.send("SERVER IS UP AND RUNNING");
 })
 
 app.listen(port, () => {
