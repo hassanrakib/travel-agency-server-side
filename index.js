dd71aa3acb8fc5789c0329c616613b6552f06969
rakib <rakiboe@gmail.com>
2021-10-31 16:59:15 +0600

    'server-on-the-way'

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId;
+const port = process.env.PORT;
+
+
+
+// middlewear
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u5kcu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database connection established...');
+        const database = client.db("travel_agency");
+        const tourPackagesCollection = database.collection("tour_packages");
+        const ordersCollection = database.collection("orders");
+
+        app.get('/tour-packages', async (req, res) => {
+            const cursor = tourPackagesCollection.find({});
+            const tourPackages = await cursor.toArray();
+            res.json(tourPackages);
+        })
+
+        app.get('/place-order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tourPackage = await tourPackagesCollection.findOne(query);
+            res.json(tourPackage);
+        })
+
+        app.post('/booking-confirm', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result.insertedId);
+        })
+
+        app.post('/new-package', async (req, res) => {
+            const newPackage = req.body;
+            const result = await tourPackagesCollection.insertOne(newPackage);
+            res.send(result.insertedId);
+        })
+
+        app.get('/my-bookings/:email', async (req, res) => {
+            const userEmail = req.params.email;
+            const query = { email: userEmail };
+            const cursor = ordersCollection.find(query);
+            const result = await cursor.toArray();
+            res.json(result);
+        })
+
+        app.delete('/cancel-booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        app.get('/all-bookings', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const allBookings = await cursor.toArray();
+            res.json(allBookings);
+        })
+
+
+    } finally {
+        //   await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log('SERVER IS RUNNING...', port);
+})
---
+++ tour-packages.js
@@ -0,0 +1,170 @@
+[
+    {
+        title: 'Climbing Switzerland Rock',
+        img: 'https://demo.moxcreative.com/avontur/wp-content/uploads/sites/12/2021/03/mountain-view-from-mount-saentis-switzerland-swiss-alps--1536x1024.jpg',
+        description: `The Western Alps (sometimes known as the Bernese Oberland region) is the largest rock climbing area in Switzerland that includes Interlaken and the famous mountains of the Eiger, Jungfrau and Monch. The limestone rock here provides many bolted single pitch and multi-pitch routes, many of which are 100's of metres long.
+        `,
+        include: [
+            'Flight Ticket',
+            '4 Nights Hotel Accomodation',
+            'All meals',
+            'Tour Guide',
+            'All transportation in destination location'
+        ],
+        exclude: [
+            'Any Private Expenses',
+            'Room Service Fees'
+        ],
+        tourGuide: {
+            name: 'Duncan R Elmore',
+            img: 'https://i.ibb.co/8jxbYfz/portrait-of-smiling-bearded-man-traveler-forester-hunter-in-the-winter-forest.jpg'
+        },
+        price: 2000,
+        details: {
+            location: 'Switzerland',
+            duration: '4 days',
+            date: 'January - May'
+        },
+        status: 'Pending'
+    },
+    {
+        title: 'Expedition Everest',
+        img: 'https://source.unsplash.com/XEW_Wd4240c/1536x1024',
+        description: `Mount Everest is Earth's highest mountain above sea level, located in the Mahalangur Himal sub-range of the Himalayas. The China–Nepal border runs across its summit point. Its elevation of 8,848.86 m was most recently established in 2020 by the Chinese and Nepali authorities.
+        `,
+        include: [
+            'Flight Ticket',
+            '4 Nights Hotel Accomodation',
+            'All meals',
+            'Tour Guide',
+            'All transportation in destination location'
+        ],
+        exclude: [
+            'Any Private Expenses',
+            'Room Service Fees'
+        ],
+        tourGuide: {
+            name: 'Danis Carl',
+            img: 'https://source.unsplash.com/xz1ZDgFq0CE/800x800'
+        },
+        price: 3000,
+        details: {
+            location: 'Nepal',
+            duration: '5 days',
+            date: 'January - March'
+        },
+        status: 'Pending'
+    },
+    {
+        title: 'Hiking Mountain',
+        img: 'https://source.unsplash.com/DijA5f0voGQ/1536x1024',
+        description: `Reaching a mountain summit requires that we confront the reflection we find in the stone mirror, working with all that is weak, lazy, and fearful within ourselves. Climbing provides a way (one way) with which to see our selves more clearly, the first step in personal growth.
+        `,
+        include: [
+            'Flight Ticket',
+            '6 Nights Hotel Accomodation',
+            'All meals',
+            'Tour Guide',
+            'All transportation in destination location'
+        ],
+        exclude: [
+            'Any Private Expenses',
+            'Room Service Fees'
+        ],
+        tourGuide: {
+            name: 'Elmore D.',
+            img: 'https://source.unsplash.com/GuEu9Y0AkBQ/800x800'
+        },
+        price: 4000,
+        details: {
+            location: 'USA',
+            duration: '6 days',
+            date: 'July - December'
+        },
+        status: 'Pending'
+    },
+    {
+        title: 'Explore Fuji Japan',
+        img: 'https://source.unsplash.com/sk34wh0H_Bg/1536x1024',
+        description: `Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains.
+        `,
+        include: [
+            'Flight Ticket',
+            '4 Nights Hotel Accomodation',
+            'All meals',
+            'Tour Guide',
+            'All transportation in destination location'
+        ],
+        exclude: [
+            'Any Private Expenses',
+            'Room Service Fees'
+        ],
+        tourGuide: {
+            name: 'Johnny Naygu',
+            img: 'https://source.unsplash.com/pVpBbIMoDBM/800x800'
+        },
+        price: 2000,
+        details: {
+            location: 'Japan',
+            duration: '4 days',
+            date: 'May - December'
+        },
+        status: 'Pending'
+    },
+    {
+        title: 'Hiking Bromo',
+        img: 'https://source.unsplash.com/7bwQXzbF6KE/1536x1024',
+        description: `After arriving in Cemoro Lawang, you can leisurely climb the Mount Bromo crater and admire the active volcano with no crowds. Mount Bromo is 2,329 meters high, but the hike to the top of the crater is not a difficult one. From the Wisma Yog hotel, where we stayed, it takes around 45 minutes one way.
+        `,
+        include: [
+            'Flight Ticket',
+            '4 Nights Hotel Accomodation',
+            'All meals',
+            'Tour Guide',
+            'All transportation in destination location'
+        ],
+        exclude: [
+            'Any Private Expenses',
+            'Room Service Fees'
+        ],
+        tourGuide: {
+            name: 'Alwxe Sr.',
+            img: 'https://unsplash.com/epRe09tKr_4'
+        },
+        price: 2000,
+        details: {
+            location: 'Indonesia',
+            duration: '4 days',
+            date: 'February - April'
+        },
+        status: 'Pending'
+    },
+    {
+        title: 'Romantic Journey',
+        img: 'https://source.unsplash.com/SIOdjcYotms/1536x1024',
+        description: `Discover the most romantic tours in Amsterdam! View the sunset on a canal tour, enjoy a walking tour, and more on Romantic Tour Amsterdam. Book online now! Romantic Tour is offering romantic getaways in Bora Bora, private tours and group tours: sunset cruise, romantic dinner, lunch on a motu.
+        `,
+        include: [
+            'Flight Ticket',
+            '4 Nights Hotel Accomodation',
+            'All meals',
+            'Tour Guide',
+            'All transportation in destination location'
+        ],
+        exclude: [
+            'Any Private Expenses',
+            'Room Service Fees'
+        ],
+        tourGuide: {
+            name: 'Ruyan Too',
+            img: 'https://source.unsplash.com/LNEMU4B9buY'
+        },
+        price: 2000,
+        details: {
+            location: 'Switzerland',
+            duration: '4 days',
+            date: 'January - May'
+        },
+        status: 'Pending'
+    },
+]
